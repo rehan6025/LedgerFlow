ac24355e5e5d12332238fff524197d12268940a3
rehan <rehanthakur6025@gmail.com>
2025-12-25 15:27:18 +0530

    2.0.2: shifting to kubernetes failed for now , I'll just use docker and docker compose now

--- user-service/user-service/src/main/java/com/ledgerflow/user_service/UserController.java
+++ user-service/user-service/src/main/java/com/ledgerflow/user_service/UserController.java
@@ -1,11 +1,13 @@
 package com.ledgerflow.user_service;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @GetMapping("/health")
